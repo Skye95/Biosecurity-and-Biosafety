4ca7289d2d428217220d7d39375b8b0a570e9130
Skye95 <100072290@students.swinburne.edu.my>
2018-04-12 00:49:51 +0800

    added swp form

--- application/controllers/swp.php
+++ application/controllers/swp.php
@@ -3,6 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
     class swp extends CI_Controller{
         
+        //load swp form
         public function index(){
             $this->load->template('swp_view');
         }
--- application/views/swp_view.php
+++ application/views/swp_view.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Biosafety and Biosecurity Online System - Annex 2</title>
+    <title>Biosafety and Biosecurity Online System - Safety Work Procedure Form</title>
     
     <style>
         body {
