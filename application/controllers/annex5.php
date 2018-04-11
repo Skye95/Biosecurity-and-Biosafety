a843a554cddcbfd0fe6b51a88aabfbca96a4bc28
Skye95 <100072290@students.swinburne.edu.my>
2018-04-12 00:47:18 +0800

    Made Annex 5 form

---
+++ application/controllers/annex5.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+    class annex5 extends CI_Controller{
+        
+        public function index(){
+            $this->load->template('annex5_view');
+        }
+        
+    }
+?>
