255d4ac00cf3aa700c86e46a73e2b5f6484cfeb0
Jasper753 <ianjasper753@gmail.com>
2018-04-23 16:14:37 +0800

    notification form update

--- application/controllers/notification_of_exporting_biological_material.php
+++ application/controllers/notification_of_exporting_biological_material.php
@@ -1,11 +1,28 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-    class notification_of_exporting_biological_material extends CI_Controller{
-        
-        public function index(){
-            $this->load->template('notification_of_exporting_biological_material_view');
+    class notification_of_exporting_biological_material extends CI_Controller
+	{
+        function __construct()
+        {
+            parent::__construct();
+            
+            $this->load->model('notification_of_exporting_biological_material_model');
         }
         
+		public function index()
+        {
+            
+            $this->form_validation->set_rules('exporter_organization','Organization', 'required|alpha');
+			
+			if($this->form_validation->run()== FALSE) 
+            {
+                $this->load->template('notification_of_exporting_biological_material_view');
+            }
+            else {
+                $this->index();
+            }
+            
+        }
     }
 ?>
