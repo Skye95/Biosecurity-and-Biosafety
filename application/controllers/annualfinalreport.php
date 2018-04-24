6371460f480ef426a107c366cc28ecc54f45677d
Jasper753 <ianjasper753@gmail.com>
2018-04-24 14:37:23 +0800

    minor fix formf and annualfinalreport form

--- application/controllers/annualfinalreport.php
+++ application/controllers/annualfinalreport.php
@@ -32,12 +32,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
             $this->form_validation->set_rules('project_summary','Project Summary', 'required');
             //5.Project Summary
             $this->form_validation->set_rules('project_outline','Explanation', 'required');
-            //6.Report incident
-            //>set_rules('project_title','incident and the actions taken', 'required');
+            //6.Report incident            
+            //>set_rules('project_incidents','incident and the actions taken', 'required');
             //7.Assessments
             //8.Change to facilities
-             //>set_rules('project_title','Building number', 'required');
-             //>set_rules('project_title','Room number', 'required');
+             //>set_rules('project_facility_changes','Building number', 'required');
+             //>set_rules('project_facility_room_number','Room number', 'required');
             //9.Sign-off
 
             
@@ -49,7 +49,38 @@ defined('BASEPATH') OR exit('No direct script access allowed');
             }
             else 
             {
-                $this-> index();    
+                 $data = array(    
+                'account_id' => $this->session->userdata('account_id'),
+                'project_title' => $this->input->post('project_title'),
+                    'chief_investigator' => $this->input->post('chief_investigator'),
+                    'personnel_extra_title' => $this->input->post('personnel_extra_title'),
+                    'personnel_extra_name' => $this->input->post('personnel_extra_name'),
+                    'personnel_extra_qualifications' => $this->input->post('personnel_extra_qualifications'),
+                    'personnel_extra_department' => $this->input->post('personnel_extra_department'),
+                    'personnel_extra_campus' => $this->input->post('personnel_extra_campus'),
+                    'personnel_extra_postal_address' => $this->input->post('personnel_extra_postal_address'),
+                    'personnel_extra_telephone' => $this->input->post('personnel_extra_telephone'),
+                    'personnel_extra_fax' => $this->input->post('personnel_extra_fax'),
+                    'personnel_extra_email_address' => $this->input->post('personnel_extra_email_address'),
+                    'project_summary' => $this->input->post('project_summary'),
+                    'project_outline' => $this->input->post('project_outline'),
+                    //'project_incidents' => $this->input->post('project_incidents'),
+                    //'project_facility_changes' => $this->input->post('project_facility_changes'),
+                    //'project_facility_room_number' => $this->input->post('project_title'),
+                    
+                );
+                if($this->annualfinalreport_model->insert_new_applicant_data($data))
+                    {
+                    
+                       $this->session->set_flashdata('msg','<div class="alert alert-danger text-center">Your Form has been Submited</div>', $data);
+                       redirect('annualfinalreport/index');
+                     
+                    } 
+                else 
+                    {                    
+                       $this->session->set_flashdata('msg','<div class="alert alert-danger text-center">An error has occured. Please try again later.</div>');
+                       redirect('annualfinalreport/index');     
+                    } 
             }
         
         }
