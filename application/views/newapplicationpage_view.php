50e9d88f381cab56132c3feb19e638f5dfd72039
Jasper753 <ianjasper753@gmail.com>
2018-04-25 17:39:29 +0800

    new application UI page

--- application/views/newapplicationpage_view.php
+++ application/views/newapplicationpage_view.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Swinburne Biosafety and Biosecurity Online System - Homepage</title>
+    <title>Swinburne Biosafety and Biosecurity Online System - New Application</title>
     
     <style>
         body {
@@ -29,18 +29,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <div class="container">
         <!-- Page Heading -->
         <h2 class="my-4 text-center">Welcome, <?php echo $this->session->userdata('account_name'); ?></h2>
-		
-        <button type="button" class="btn" onclick="window.location='<?php echo base_url("index.php/home");?>'">Return</button>		
-		<h3>Application Page</h3>
+
+        <button type="button" class="btn" onclick="window.location='<?php echo base_url("index.php/applicationpage");?>'">Return</button>		
+		<h3>New Application Page</h3>
 		<hr>
 	<div class="row" >
 	<div class="col-lg-6" >
 		<div>		
 			<div class="row">
 				<div class="col-md-7">
-					<a href="<?php echo base_url(); ?>index.php/newapplicationpage">
-						<img class="img-fluid rounded mb-3 mb-md-0" src="<?php echo base_url('assets\images\ApplicantForm\New Application.jpg') ?>" 
-						alt="" title="New Application">
+					<a href="#">
+						<img class="img-fluid rounded mb-3 mb-md-0" src="<?php echo base_url('assets\images\ApplicantForm\Living Modified Organism.jpg') ?>" 
+						alt="" title="Living Modified Organism (LMO)">
 					</a>
 				</div>				
 			</div>
@@ -52,8 +52,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 			<div class="row">
 				<div class="col-md-7">
 					<a href="#">
-						<img class="img-fluid rounded mb-3 mb-md-0" src="<?php echo base_url('assets\images\ApplicantForm\Modification of Approved Project.jpg') ?>" 
-						alt=""title="Modification of Approved Project">
+						<img class="img-fluid rounded mb-3 mb-md-0" src="<?php echo base_url('assets\images\ApplicantForm\Exempt Dealing.jpg') ?>" 
+						alt=""title="Exempt Dealing">
 					</a>
 				</div>				
 			</div>
@@ -65,8 +65,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 			<div class="row">
 				<div class="col-md-7">
 					<a href="#">
-						<img class="img-fluid rounded mb-3 mb-md-0" src="<?php echo base_url('assets\images\ApplicantForm\ExtensionTermination of Approved Project.jpg') ?>" 
-						alt="" title="Extension/Termination of Approved Project">
+						<img class="img-fluid rounded mb-3 mb-md-0" src="<?php echo base_url('assets\images\ApplicantForm\Biohazardous Material.jpg') ?>" 
+						alt="" title="Biohazardous Material">
 					</a>
 				</div>				
 			</div>
@@ -81,7 +81,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 			<div>		
 				<div class="row">
 					<div class="card my-4">
-						<h5 class="card-header">LMO Definition</h5>
+						<h5 class="card-header">Form Guide</h5>
 						<div class="card-body">
 						test test test 1131231231231231231231231231231212331121321321321213213213 test test test 1131231231231231231231231231231212331121321321321213213213 
 						test test test 1131231231231231231231231231231212331121321321321213213213 test test test 1131231231231231231231231231231212331121321321321213213213 
@@ -92,7 +92,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 			<div>		
 				<div class="row">
 					<div class="card my-4">
-						<h5 class="card-header">Type of LMO</h5>
+						<h5 class="card-header">Reminder</h5>
 						<div class="card-body">
 						test test test 1131231231231231231231231231231212331121321321321213213213 test test test 1131231231231231231231231231231212331121321321321213213213 
 						test test test 1131231231231231231231231231231212331121321321321213213213 test test test 1131231231231231231231231231231212331121321321321213213213 
