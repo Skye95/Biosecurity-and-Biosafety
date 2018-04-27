2c835077377d64e5a0fa62b9ce2e3c334dc9534a
Skye95 <100072290@students.swinburne.edu.my>
2018-04-27 18:45:19 +0800

    notification of exporting biological material form done

--- application/models/notification_of_LMO_and_BM_model.php
+++ application/models/notification_of_LMO_and_BM_model.php
@@ -8,7 +8,7 @@ class notification_of_LMO_and_BM_model extends CI_Model
         parent::__construct();
     }
 	
-    # Retrieves Annex 2 form by users ID for now
+    
 	function get_form_by_id($id)
 	{
 		$this->db->where('account_id', $id);
