0f48bad753033f40981009be93d31fdc8626ffcd
professorgene <werewolf.xeno@yahoo.com>
2018-04-29 23:41:25 +0800

    updated approval to include remarks

--- application/controllers/accountapproval.php
+++ application/controllers/accountapproval.php
@@ -34,6 +34,7 @@ class accountapproval extends CI_Controller {
     {
         #$data['readnotif'] = $this->notification_model->get_read( $this->session->userdata('account_id') );
         $id = $this->uri->segment(3);
+        #$id = $this->uri->segment(4);
         $this->account_model->update_approval($id, 0);
         
         redirect('accountapproval/index');
--- application/views/adminpage_view.php
+++ application/views/adminpage_view.php
@@ -44,34 +44,34 @@ if($this->session->userdata('account_type') != 2){
         
         <div class="row">
             <div class="col-lg-3 col-md-4 col-sm-6 col-6 portfolio-item">
-                <div class="card card-block h-50 justify-content-center align-items-center">
+                <div class="card card-block justify-content-center align-items-center">
                     <a href="<?php echo base_url(); ?>index.php/accountapproval/index"><img class="card-img-top" src="<?php echo base_url('assets\images\ApplicantForm\History.jpg') ?>" alt=""></a>
                     <div class="card-body">
-                        <h5 class="card-title">
+                        <h6 class="card-title">
                             <a href="<?php echo base_url(); ?>index.php/accountapproval/index">Account Approvals</a>
-                        </h5>
+                        </h6>
                         <p class="card-text"></p>
                     </div>
                 </div>
             </div>
             <div class="col-lg-3 col-md-4 col-sm-6 col-6 portfolio-item">
-                <div class="card card-block h-50 justify-content-center align-items-center">
+                <div class="card card-block justify-content-center align-items-center">
                     <a href="#"><img class="card-img-top" src="<?php echo base_url('assets\images\ApplicantForm\History.jpg') ?>" alt=""></a>
                     <div class="card-body">
-                        <h5 class="card-title">
+                        <h6 class="card-title">
                             <a href="#">Application Approvals</a>
-                        </h5>
+                        </h6>
                         <p class="card-text"></p>
                     </div>
                 </div>
             </div>
             <div class="col-lg-3 col-md-4 col-sm-6 col-6 portfolio-item">
-                <div class="card card-block h-50 justify-content-center align-items-center">
+                <div class="card card-block justify-content-center align-items-center">
                     <a href="#"><img class="card-img-top" src="<?php echo base_url('assets\images\ApplicantForm\History.jpg') ?>" alt=""></a>
                     <div class="card-body">
-                        <h5 class="card-title">
+                        <h6 class="card-title">
                             <a href="#">LMO and Biohazardous Materials Applications</a>
-                        </h5>
+                        </h6>
                         <p class="card-text"></p>
                     </div>
                 </div>
