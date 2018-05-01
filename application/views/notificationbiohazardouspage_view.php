8644270e056c3f3dd4d52bc605e246d6d594c7ce
Jasper753 <ianjasper753@gmail.com>
2018-05-01 21:40:42 +0800

    slide for biohazard materials

--- application/views/notificationbiohazardouspage_view.php
+++ application/views/notificationbiohazardouspage_view.php
@@ -49,30 +49,52 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		
 		<div class="col-lg-7">
 			<div>		
-				<h4>Fun Fact</h4>
+				<h4>Announcements</h4>
 			</div>				
-			<div>		
-				<div class="row">
-					<div class="card my-4">
-						<h5 class="card-header">Did you know? The human saliva</h5>
-						<div class="card-body">
-						<img class="img-fluid rounded mb-3 mb-md-0" src="<?php echo base_url('assets\images\announcement\funfact1.png') ?>" 
-						alt="" title="The human saliva"> 
-						</div>
-					</div>				
+			<div class="row">
+          <div id="carouselExampleIndicators" class="carousel slide my-4" data-ride="carousel">
+            <ol class="carousel-indicators">
+              <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="6"></li>
+            </ol>
+			<div class="carousel-inner" role="listbox">
+              <div class="carousel-item active">
+                <img class="d-block img-fluid" src="<?php echo base_url('assets\images\4_biohazardousmaterials\bm1.jpg') ?>" alt="First slide">
+			  </div>
+              <div class="carousel-item">
+                <img class="d-block img-fluid" src="<?php echo base_url('assets\images\4_biohazardousmaterials\bm2.jpg') ?>" alt="Second slide">
+			  </div>
+                <div class="carousel-item">
+                <img class="d-block img-fluid" src="<?php echo base_url('assets\images\4_biohazardousmaterials\bm3.jpg') ?>" alt="Second slide">
+			  </div>
+                <div class="carousel-item">
+                <img class="d-block img-fluid" src="<?php echo base_url('assets\images\4_biohazardousmaterials\bm4.jpg') ?>" alt="Second slide">
+			  </div>
+                <div class="carousel-item">
+                <img class="d-block img-fluid" src="<?php echo base_url('assets\images\4_biohazardousmaterials\bm5.jpg') ?>" alt="Second slide">
+			  </div>
+                <div class="carousel-item">
+                <img class="d-block img-fluid" src="<?php echo base_url('assets\images\4_biohazardousmaterials\bm6.jpg') ?>" alt="Second slide">
+			  </div>
+                <div class="carousel-item">
+                <img class="d-block img-fluid" src="<?php echo base_url('assets\images\4_biohazardousmaterials\bm7.jpg') ?>" alt="Second slide">
+			  </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+            </a>
+          </div>				
 				</div>
-			</div>
-			<div>		
-				<div class="row">
-					<div class="card my-4">
-						<h5 class="card-header">Reminder</h5>
-						<div class="card-body">
-						test test test 1131231231231231231231231231231212331121321321321213213213 test test test 1131231231231231231231231231231212331121321321321213213213 
-						test test test 1131231231231231231231231231231212331121321321321213213213 test test test 1131231231231231231231231231231212331121321321321213213213 
-						</div>
-					</div>				
-				</div>
-			</div>
 		</div>
 	</div>					
 	</div>
