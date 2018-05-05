36f93a65bb053366fd3a670dc7a1e639aad17ab2
professorgene <werewolf.xeno@yahoo.com>
2018-05-05 22:42:54 +0800

    history fix

--- application/models/history_model.php
+++ application/models/history_model.php
@@ -26,179 +26,212 @@ class history_model extends CI_Model
         $this->load->model('swp_model');
     }
 	
-    # Retrieves Annex 2 form by users ID for now
 	function get_all_form_by_id($id)
 	{
         $result = [];
         
         $i = $this->annex2_model->get_form_by_account_id($id);
-        foreach ($i as $row) {
-            $data = [
-                'type' => 'ANNEX 2 FORM',
-                'application_id' => $row->application_id,
-                'approval' => $row->application_approved
-            ];
-            array_push($result, $data);
+        if(count((array)$i) > 0){
+            foreach ($i as $row) {
+                $data = [
+                    'type' => 'ANNEX 2 FORM',
+                    'application_id' => $row->application_id,
+                    'approval' => $row->application_approved
+                ];
+                array_push($result, $data);
+            }
         }
         
         $i = $this->annex3_model->get_form_by_account_id($id);
-        foreach ($i as $row) {
-            $data = [
-                'type' => 'ANNEX 3 FORM',
-                'application_id' => $row->application_id,
-                'approval' => $row->application_approved
-            ];
-            array_push($result, $data);
+        if(count((array)$i) > 0){
+            foreach ($i as $row) {
+                $data = [
+                    'type' => 'ANNEX 3 FORM',
+                    'application_id' => $row->application_id,
+                    'approval' => $row->application_approved
+                ];
+                array_push($result, $data);
+            }
         }
         
         $i = $this->annex4_model->get_form_by_account_id($id);
-        foreach ($i as $row) {
-            $data = [
-                'type' => 'ANNEX 4 FORM',
-                'application_id' => $row->application_id,
-                'approval' => $row->application_approved
-            ];
-            array_push($result, $data);
+        if(count((array)$i) > 0){
+            foreach ($i as $row) {
+                $data = [
+                    'type' => 'ANNEX 4 FORM',
+                    'application_id' => $row->application_id,
+                    'approval' => $row->application_approved
+                ];
+                array_push($result, $data);
+            }
         }
         
         $i = $this->annex5_model->get_form_by_account_id($id);
-        foreach ($i as $row) {
-            $data = [
-                'type' => 'ANNEX 5 FORM',
-                'application_id' => $row->application_id,
-                'approval' => $row->application_approved
-            ];
-            array_push($result, $data);
+        if(count((array)$i) > 0){
+            foreach ($i as $row) {
+                $data = [
+                    'type' => 'ANNEX 5 FORM',
+                    'application_id' => $row->application_id,
+                    'approval' => $row->application_approved
+                ];
+                array_push($result, $data);
+            }
         }
         
         $i = $this->annualfinalreport_model->get_form_by_account_id($id);
-        foreach ($i as $row) {
-            $data = [
-                'type' => 'SBC ANNUAL OR FINAL REPORT FOR USE OF BIOHAZARDOUS MATERIALS',
-                'application_id' => $row->application_id,
-                'approval' => $row->application_approved
-            ];
-            array_push($result, $data);
+        if(count((array)$i) > 0){
+            foreach ($i as $row) {
+                $data = [
+                    'type' => 'SBC ANNUAL OR FINAL REPORT FOR USE OF BIOHAZARDOUS MATERIALS',
+                    'application_id' => $row->application_id,
+                    'approval' => $row->application_approved
+                ];
+                array_push($result, $data);
+            }
         }
         
         $i = $this->biohazard_model->get_form_by_account_id($id);
-        foreach ($i as $row) {
-            $data = [
-                'type' => 'APPLICATION FOR BIOSAFETY CLEARANCE FORM',
-                'application_id' => $row->application_id,
-                'approval' => $row->application_approved
-            ];
-            array_push($result, $data);
+        if(count((array)$i) > 0){
+            foreach ($i as $row) {
+                $data = [
+                    'type' => 'APPLICATION FOR BIOSAFETY CLEARANCE FORM',
+                    'application_id' => $row->application_id,
+                    'approval' => $row->application_approved
+                ];
+                array_push($result, $data);
+            }
         }
         
         $i = $this->exempt_model->get_form_by_account_id($id);
-        foreach ($i as $row) {
-            $data = [
-                'type' => 'APPLICATION FOR BIOSAFETY CLEARANCE EXEMPT DEALINGS FORM',
-                'application_id' => $row->application_id,
-                'approval' => $row->application_approved
-            ];
-            array_push($result, $data);
+        if(count((array)$i) > 0){
+            foreach ($i as $row) {
+                $data = [
+                    'type' => 'APPLICATION FOR BIOSAFETY CLEARANCE EXEMPT DEALINGS FORM',
+                    'application_id' => $row->application_id,
+                    'approval' => $row->application_approved
+                ];
+                array_push($result, $data);
+            }
         }
         
         $i = $this->forme_model->get_form_by_account_id($id);
-        foreach ($i as $row) {
-            $data = [
-                'type' => 'FORM E',
-                'application_id' => $row->application_id,
-                'approval' => $row->application_approved
-            ];
-            array_push($result, $data);
+        if(count((array)$i) > 0){
+            foreach ($i as $row) {
+                $data = [
+                    'type' => 'FORM E',
+                    'application_id' => $row->application_id,
+                    'approval' => $row->application_approved
+                ];
+                array_push($result, $data);
+            }
         }
         
         $i = $this->formf_model->get_form_by_account_id($id);
-        foreach ($i as $row) {
-            $data = [
-                'type' => 'FORM F',
-                'application_id' => $row->application_id,
-                'approval' => $row->application_approved
-            ];
-            array_push($result, $data);
+        if(count((array)$i) > 0){
+            foreach ($i as $row) {
+                $data = [
+                    'type' => 'FORM F',
+                    'application_id' => $row->application_id,
+                    'approval' => $row->application_approved
+                ];
+                array_push($result, $data);
+            }
         }
         
         $i = $this->hirarc_model->get_form_by_account_id($id);
-        foreach ($i as $row) {
-            $data = [
-                'type' => 'OHS-F-4.5.X HIRARC FORM',
-                'application_id' => $row->application_id,
-                'approval' => $row->application_approved
-            ];
-            array_push($result, $data);
+        if(count((array)$i) > 0){
+            foreach ($i as $row) {
+                $data = [
+                    'type' => 'OHS-F-4.5.X HIRARC FORM',
+                    'application_id' => $row->application_id,
+                    'approval' => $row->application_approved
+                ];
+                array_push($result, $data);
+            }
         }
         
         $i = $this->incidentaccidentreport_model->get_form_by_account_id($id);
-        foreach ($i as $row) {
-            $data = [
-                'type' => 'OHS-F-4.20.X INCIDENT ACCIDENT REPORT',
-                'application_id' => $row->application_id,
-                'approval' => $row->application_approved
-            ];
-            array_push($result, $data);
+        if(count((array)$i) > 0){
+            foreach ($i as $row) {
+                $data = [
+                    'type' => 'OHS-F-4.20.X INCIDENT ACCIDENT REPORT',
+                    'application_id' => $row->application_id,
+                    'approval' => $row->application_approved
+                ];
+                array_push($result, $data);
+            }
         }
         
         $i = $this->notification_of_exporting_biological_material_model->get_form_by_account_id($id);
-        foreach ($i as $row) {
-            $data = [
-                'type' => 'SSBC NOTIFICATION OF EXPORTING LMO AND BIOHAZARDOUS MATERIAL',
-                'application_id' => $row->application_id,
-                'approval' => $row->application_approved
-            ];
-            array_push($result, $data);
+        if(count((array)$i) > 0){
+            foreach ($i as $row) {
+                $data = [
+                    'type' => 'SSBC NOTIFICATION OF EXPORTING LMO AND BIOHAZARDOUS MATERIAL',
+                    'application_id' => $row->application_id,
+                    'approval' => $row->application_approved
+                ];
+                array_push($result, $data);
+            }
         }
         
         $i = $this->notification_of_LMO_and_BM_model->get_form_by_account_id($id);
-        foreach ($i as $row) {
-            $data = [
-                'type' => 'NOTIFICATION OF LMO AND BIOHAZARDOUS MATERIAL',
-                'application_id' => $row->application_id,
-                'approval' => $row->application_approved
-            ];
-            array_push($result, $data);
+        if(count((array)$i) > 0){
+            foreach ($i as $row) {
+                $data = [
+                    'type' => 'NOTIFICATION OF LMO AND BIOHAZARDOUS MATERIAL',
+                    'application_id' => $row->application_id,
+                    'approval' => $row->application_approved
+                ];
+                array_push($result, $data);
+            }
         }
         
         $i = $this->pc1_model->get_form_by_account_id($id);
-        foreach ($i as $row) {
-            $data = [
-                'type' => 'APPLICATION FOR NLRDS SUITABLE FOR PC1 FORM',
-                'application_id' => $row->application_id,
-                'approval' => $row->application_approved
-            ];
-            array_push($result, $data);
+        if(count((array)$i) > 0){
+            foreach ($i as $row) {
+                $data = [
+                    'type' => 'APPLICATION FOR NLRDS SUITABLE FOR PC1 FORM',
+                    'application_id' => $row->application_id,
+                    'approval' => $row->application_approved
+                ];
+                array_push($result, $data);
+            }
         }
         
         $i = $this->pc2_model->get_form_by_account_id($id);
-        foreach ($i as $row) {
-            $data = [
-                'type' => 'APPLICATION FOR NLRDS SUITABLE FOR PC2 FORM',
-                'application_id' => $row->application_id,
-                'approval' => $row->application_approved
-            ];
+        if(count((array)$i) > 0){
+            foreach ($i as $row) {
+                $data = [
+                    'type' => 'APPLICATION FOR NLRDS SUITABLE FOR PC2 FORM',
+                    'application_id' => $row->application_id,
+                    'approval' => $row->application_approved
+                ];
+                array_push($result, $data);
+            }
         }
-            array_push($result, $data);
         
         $i = $this->procurement_model->get_form_by_account_id($id);
-        foreach ($i as $row) {
-            $data = [
-                'type' => 'OHS-F-4.18.X PRE-PURCHASE MATERIAL RISK ASSESSMENT',
-                'application_id' => $row->application_id,
-                'approval' => $row->application_approved
-            ];
-            array_push($result, $data);
+        if(count((array)$i) > 0){
+            foreach ($i as $row) {
+                $data = [
+                    'type' => 'OHS-F-4.18.X PRE-PURCHASE MATERIAL RISK ASSESSMENT',
+                    'application_id' => $row->application_id,
+                    'approval' => $row->application_approved
+                ];
+                array_push($result, $data);
+            }
         }
         
         $i = $this->swp_model->get_form_by_account_id($id);
-        foreach ($i as $row) {
-            $data = [
-                'type' => 'SSBC SAFE WORK PROCEDURE',
-                'application_id' => $row->application_id,
-                'approval' => $row->application_approved
-            ];
-            array_push($result, $data);
+        if(count((array)$i) > 0){
+            foreach ($i as $row) {
+                $data = [
+                    'type' => 'SSBC SAFE WORK PROCEDURE',
+                    'application_id' => $row->application_id,
+                    'approval' => $row->application_approved
+                ];
+                array_push($result, $data);
+            }
         }
         
 		return $result;
--- application/views/history_view.php
+++ application/views/history_view.php
@@ -24,128 +24,127 @@ if(!$this->session->userdata('isLogin')){
     <!-- Navigation -->
     <?php include_once 'template/navbar.php' ?>
 
+    <?php 
+        if(!isset($past)) {
+            $past = null;
+        } 
+    ?>
     <!-- Page Content -->
     <div class="container">
         <!-- Page Heading -->
-        <div class="container">
-            <h5>Submitted or Past Applications</h5>
-            <br/>
-            <input class="form-control" id="searchbar" type="text" placeholder="Search here">
-            <div class="row">
-                <div class="col-md-1">
-                </div>
-                <div class="col-md-10 text-center">
-                    <br/>
-                    <?php echo $this->session->flashdata('msg'); ?>
-                </div>
-                <div class="col-md-1">
-                </div>
+        <h5>Submitted or Past Applications</h5>
+        <br/>
+        <input class="form-control" id="searchbar" type="text" placeholder="Search here">
+        <div class="row">
+            <div class="col-md-1">
             </div>
-
-            <div class="table-responsive">
-                <table class="table table-hover table-bordered">
-                    <thead>
-                        <tr>
-                            <th></th>
-                            <th>Application Type</th>
-                            <th>Approval Progress</th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody id="inventory">
-                    <?php if(isset($past)) { ?>
-                        <?php $i=0; foreach(/*array_reverse($past)*/ $past as $row): ?>
-                        <tr class="searchable">
-                            <td class="text-center"><?php echo $i = $i + 1; ?></td>
-                            <td><?php echo $row['type']; ?></td>
-                            <td class="text-center"><?php 
-                                if($row['approval'] < 3 || $row['approval'] == null){
-                                    echo "Awaiting Approval";
-                                } else {
-                                    echo "Approved";
-                                }
-                                ?>
-                            </td>
-                            <td class="text-center">
-                                <i class="fa fa-bars btn btn-info" onclick="view_application(<?php echo $row['application_id']; ?>, '<?php echo $row['type']; ?>')" title="Details"></i>
-                                <i class="fa fa-edit btn btn-warning" onclick="edit_application(<?php echo $row['application_id']; ?>, '<?php echo $row['type']; ?>')" title="Edit"></i>
-                                <i class="fa fa-clock btn btn-primary" onclick="extend_application()" title="Extend"></i>
-                                <i class="fa fa-times btn btn-danger" onclick="terminate_application()" title="Terminate"></i>
-                            </td>
-                        </tr>
-                        <?php endforeach; ?>
-                    <?php } else { ?>
-                    
-                    <?php } ?>
-                    </tbody>
-                </table>
+            <div class="col-md-10 text-center">
+                <br/>
+                <?php echo $this->session->flashdata('msg'); ?>
             </div>
+            <div class="col-md-1">
+            </div>
+        </div>
 
-            <script>
-                $(document).ready(function(){
-                    $("#searchbar").on("keyup", function() {
-                        var value = $(this).val().toLowerCase();
-                        $("#inventory .searchable").filter(function() {
-                            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-                        });
+        <div class="table-responsive">
+            <table class="table table-hover table-bordered">
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>Application Type</th>
+                        <th>Approval Progress</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody id="inventory">
+                    <?php $i=0; foreach(/*array_reverse($past)*/ $past as $row): ?>
+                    <tr class="searchable">
+                        <td class="text-center"><?php echo $i = $i + 1; ?></td>
+                        <td><?php echo $row['type']; ?></td>
+                        <td class="text-center"><?php 
+                            if($row['approval'] < 3 || $row['approval'] == null){
+                                echo "Awaiting Approval";
+                            } else {
+                                echo "Approved";
+                            }
+                            ?>
+                        </td>
+                        <td class="text-center">
+                            <i class="fa fa-bars btn btn-info" onclick="view_application(<?php echo $row['application_id']; ?>, '<?php echo $row['type']; ?>')" title="Details"></i>
+                            <i class="fa fa-edit btn btn-warning" onclick="edit_application(<?php echo $row['application_id']; ?>, '<?php echo $row['type']; ?>')" title="Edit"></i>
+                            <i class="fa fa-clock btn btn-primary" onclick="extend_application()" title="Extend"></i>
+                            <i class="fa fa-times btn btn-danger" onclick="terminate_application()" title="Terminate"></i>
+                        </td>
+                    </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
+
+        <script>
+            $(document).ready(function(){
+                $("#searchbar").on("keyup", function() {
+                    var value = $(this).val().toLowerCase();
+                    $("#inventory .searchable").filter(function() {
+                        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
                     });
                 });
-            </script>
+            });
+        </script>
 
-            <script>
-                function view_application(i, j){
-                    if(j == "ANNEX 2 FORM"){
-                        window.location = "<?php echo base_url(); ?>index.php/annex2/load_form?id=" + i;
-                    } else if (j == "ANNEX 3 FORM"){
-                        window.location = "<?php echo base_url(); ?>index.php/annex3/load_form?id=" + i;
-                    } else if (j == "ANNEX 4 FORM"){
-                        window.location = "<?php echo base_url(); ?>index.php/annex4/load_form?id=" + i;
-                    } else if (j == "ANNEX 5 FORM"){
-                        window.location = "<?php echo base_url(); ?>index.php/annex5/load_form?id=" + i;
-                    } else if (j == "SBC ANNUAL OR FINAL REPORT FOR USE OF BIOHAZARDOUS MATERIALS"){
-                        window.location = "<?php echo base_url(); ?>index.php/annualfinalreport/load_form?id=" + i;
-                    } else if (j == "APPLICATION FOR BIOSAFETY CLEARANCE FORM"){
-                        window.location = "<?php echo base_url(); ?>index.php/biohazard/load_form?id=" + i;
-                    } else if (j == "APPLICATION FOR BIOSAFETY CLEARANCE EXEMPT DEALINGS FORM"){
-                        window.location = "<?php echo base_url(); ?>index.php/exempt/load_form?id=" + i;
-                    } else if (j == "FORM E"){
-                        window.location = "<?php echo base_url(); ?>index.php/forme/load_form?id=" + i;
-                    } else if (j == "FORM F"){
-                        window.location = "<?php echo base_url(); ?>index.php/formf/load_form?id=" + i;
-                    } else if (j == "OHS-F-4.5.X HIRARC FORM"){
-                        window.location = "<?php echo base_url(); ?>index.php/hirarc/load_form?id=" + i;
-                    } else if (j == "OHS-F-4.20.X INCIDENT ACCIDENT REPORT"){
-                        window.location = "<?php echo base_url(); ?>index.php/incidentaccidentreport/load_form?id=" + i;
-                    } else if (j == "SSBC NOTIFICATION OF EXPORTING LMO AND BIOHAZARDOUS MATERIAL"){
-                        window.location = "<?php echo base_url(); ?>index.php/notification_of_exporting_biological_material/load_form?id=" + i;
-                    } else if (j == "NOTIFICATION OF LMO AND BIOHAZARDOUS MATERIAL"){
-                        window.location = "<?php echo base_url(); ?>index.php/notification_of_LMO_and_BM/load_form?id=" + i;
-                    } else if (j == "APPLICATION FOR NLRDS SUITABLE FOR PC1 FORM"){
-                        window.location = "<?php echo base_url(); ?>index.php/pc1/load_form?id=" + i;
-                    } else if (j == "APPLICATION FOR NLRDS SUITABLE FOR PC2 FORM"){
-                        window.location = "<?php echo base_url(); ?>index.php/pc2/load_form?id=" + i;
-                    } else if (j == "OHS-F-4.18.X PRE-PURCHASE MATERIAL RISK ASSESSMENT"){
-                        window.location = "<?php echo base_url(); ?>index.php/procurement/load_form?id=" + i;
-                    } else if (j == "SSBC SAFE WORK PROCEDURE"){
-                        window.location = "<?php echo base_url(); ?>index.php/swp/load_form?id=" + i;
-                    }
+        <script>
+            function view_application(i, j){
+                if(j == "ANNEX 2 FORM"){
+                    window.location = "<?php echo base_url(); ?>index.php/annex2/load_form?id=" + i;
+                } else if (j == "ANNEX 3 FORM"){
+                    window.location = "<?php echo base_url(); ?>index.php/annex3/load_form?id=" + i;
+                } else if (j == "ANNEX 4 FORM"){
+                    window.location = "<?php echo base_url(); ?>index.php/annex4/load_form?id=" + i;
+                } else if (j == "ANNEX 5 FORM"){
+                    window.location = "<?php echo base_url(); ?>index.php/annex5/load_form?id=" + i;
+                } else if (j == "SBC ANNUAL OR FINAL REPORT FOR USE OF BIOHAZARDOUS MATERIALS"){
+                    window.location = "<?php echo base_url(); ?>index.php/annualfinalreport/load_form?id=" + i;
+                } else if (j == "APPLICATION FOR BIOSAFETY CLEARANCE FORM"){
+                    window.location = "<?php echo base_url(); ?>index.php/biohazard/load_form?id=" + i;
+                } else if (j == "APPLICATION FOR BIOSAFETY CLEARANCE EXEMPT DEALINGS FORM"){
+                    window.location = "<?php echo base_url(); ?>index.php/exempt/load_form?id=" + i;
+                } else if (j == "FORM E"){
+                    window.location = "<?php echo base_url(); ?>index.php/forme/load_form?id=" + i;
+                } else if (j == "FORM F"){
+                    window.location = "<?php echo base_url(); ?>index.php/formf/load_form?id=" + i;
+                } else if (j == "OHS-F-4.5.X HIRARC FORM"){
+                    window.location = "<?php echo base_url(); ?>index.php/hirarc/load_form?id=" + i;
+                } else if (j == "OHS-F-4.20.X INCIDENT ACCIDENT REPORT"){
+                    window.location = "<?php echo base_url(); ?>index.php/incidentaccidentreport/load_form?id=" + i;
+                } else if (j == "SSBC NOTIFICATION OF EXPORTING LMO AND BIOHAZARDOUS MATERIAL"){
+                    window.location = "<?php echo base_url(); ?>index.php/notification_of_exporting_biological_material/load_form?id=" + i;
+                } else if (j == "NOTIFICATION OF LMO AND BIOHAZARDOUS MATERIAL"){
+                    window.location = "<?php echo base_url(); ?>index.php/notification_of_LMO_and_BM/load_form?id=" + i;
+                } else if (j == "APPLICATION FOR NLRDS SUITABLE FOR PC1 FORM"){
+                    window.location = "<?php echo base_url(); ?>index.php/pc1/load_form?id=" + i;
+                } else if (j == "APPLICATION FOR NLRDS SUITABLE FOR PC2 FORM"){
+                    window.location = "<?php echo base_url(); ?>index.php/pc2/load_form?id=" + i;
+                } else if (j == "OHS-F-4.18.X PRE-PURCHASE MATERIAL RISK ASSESSMENT"){
+                    window.location = "<?php echo base_url(); ?>index.php/procurement/load_form?id=" + i;
+                } else if (j == "SSBC SAFE WORK PROCEDURE"){
+                    window.location = "<?php echo base_url(); ?>index.php/swp/load_form?id=" + i;
                 }
+            }
 
-                function edit_application(i, j){
-                    if(confirm("You will be submitting an edit request to the corresponding officer. Are you sure?")){
-                        /* window.location = "<?php echo base_url(); ?>index.php/annex2/load_form/" + i; */
-                    }
+            function edit_application(i, j){
+                if(confirm("You will be submitting an edit request to the corresponding officer. Are you sure?")){
+                    /* window.location = "<?php echo base_url(); ?>index.php/annex2/load_form/" + i; */
                 }
+            }
 
-                function extend_application(){
-                     window.location = "<?php echo base_url(); ?>index.php/annex5/";
-                }
-                
-                function terminate_application(){
-                     window.location = "<?php echo base_url(); ?>index.php/annex5/";
-                }
-            </script>
-        </div>
+            function extend_application(){
+                 window.location = "<?php echo base_url(); ?>index.php/annex5/";
+            }
+
+            function terminate_application(){
+                 window.location = "<?php echo base_url(); ?>index.php/annex5/";
+            }
+        </script>
     </div>
 </body>
 </html>
