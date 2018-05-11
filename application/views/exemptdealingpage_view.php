92f3221eeee5ac0397f4cb51c58b25a7c13ecc40
Jasper753 <ianjasper753@gmail.com>
2018-05-11 18:39:28 +0800

    Fix merge conflict redo Carousel

--- application/views/exemptdealingpage_view.php
+++ application/views/exemptdealingpage_view.php
@@ -80,35 +80,32 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 				  <div class="carousel-item">
 					<img class="d-block img-fluid" src="<?php echo base_url('assets\images\3_exemptedactivities\ea2.jpg') ?>" alt="Second slide">
 				  </div>
-				</div>
-				<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-				  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-				  <span class="sr-only">Previous</span>
-				</a>
-				<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-				  <span class="carousel-control-next-icon" aria-hidden="true"></span>
-				  <span class="sr-only">Next</span>
-				</a>
-			  </div>				
-			</div>
-		</div>
-		<div class="col-lg-12">
-		<hr>
-			<div>		
-				<h4>Contained use activities involving LMOs exempted from Notification Application</h4>
-			</div>				
-			<div class="row">
-			  <div id="carouselExampleIndicators" class="carousel slide my-3" data-ride="carousel">
-				<ol class="carousel-indicators">
-				  <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-				  <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-				</ol>
-				<div class="carousel-inner" role="listbox">
-				  <div class="carousel-item active">
-					<img class="d-block img-fluid" src="<?php echo base_url('assets\images\4_biohazardousmaterials\bm1.jpg') ?>" alt="First slide">
+                  <div class="carousel-item">
+					<img class="d-block img-fluid" src="<?php echo base_url('assets\images\3_exemptedactivities\ea3.jpg') ?>" alt="Second slide">
 				  </div>
-				  <div class="carousel-item">
-					<img class="d-block img-fluid" src="<?php echo base_url('assets\images\4_biohazardousmaterials\bm2.jpg') ?>" alt="Second slide">
+                    <div class="carousel-item">
+					<img class="d-block img-fluid" src="<?php echo base_url('assets\images\3_exemptedactivities\ea4.jpg') ?>" alt="Second slide">
+				  </div>
+                    <div class="carousel-item">
+					<img class="d-block img-fluid" src="<?php echo base_url('assets\images\3_exemptedactivities\ea5.jpg') ?>" alt="Second slide">
+				  </div>
+                    <div class="carousel-item">
+					<img class="d-block img-fluid" src="<?php echo base_url('assets\images\3_exemptedactivities\ea6.jpg') ?>" alt="Second slide">
+				  </div>
+                    <div class="carousel-item">
+					<img class="d-block img-fluid" src="<?php echo base_url('assets\images\3_exemptedactivities\ea7.jpg') ?>" alt="Second slide">
+				  </div>
+                    <div class="carousel-item">
+					<img class="d-block img-fluid" src="<?php echo base_url('assets\images\3_exemptedactivities\ea8.jpg') ?>" alt="Second slide">
+				  </div>
+                    <div class="carousel-item">
+					<img class="d-block img-fluid" src="<?php echo base_url('assets\images\3_exemptedactivities\ea9.jpg') ?>" alt="Second slide">
+				  </div>
+                    <div class="carousel-item">
+					<img class="d-block img-fluid" src="<?php echo base_url('assets\images\3_exemptedactivities\ea10.jpg') ?>" alt="Second slide">
+				  </div>
+                    <div class="carousel-item">
+					<img class="d-block img-fluid" src="<?php echo base_url('assets\images\3_exemptedactivities\ea11.jpg') ?>" alt="Second slide">
 				  </div>
 				</div>
 				<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
