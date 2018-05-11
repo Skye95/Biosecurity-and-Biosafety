6da6dd068bb4f1faa22a6aedb28fddd4b496d2ef
Jasper753 <ianjasper753@gmail.com>
2018-05-11 18:43:31 +0800

    material biohazard page

--- application/views/notificationbiohazardouspage_view.php
+++ application/views/notificationbiohazardouspage_view.php
@@ -51,50 +51,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 			<div>		
 				<h4>Announcements</h4>
 			</div>				
-			<div class="row">
-          <div id="carouselExampleIndicators" class="carousel slide my-4" data-ride="carousel">
-            <ol class="carousel-indicators">
-              <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-              <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-              <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-              <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
-              <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
-              <li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
-              <li data-target="#carouselExampleIndicators" data-slide-to="6"></li>
-            </ol>
-			<div class="carousel-inner" role="listbox">
-              <div class="carousel-item active">
-                <img class="d-block img-fluid" src="<?php echo base_url('assets\images\4_biohazardousmaterials\bm1.jpg') ?>" alt="First slide">
-			  </div>
-              <div class="carousel-item">
-                <img class="d-block img-fluid" src="<?php echo base_url('assets\images\4_biohazardousmaterials\bm2.jpg') ?>" alt="Second slide">
-			  </div>
-                <div class="carousel-item">
-                <img class="d-block img-fluid" src="<?php echo base_url('assets\images\4_biohazardousmaterials\bm3.jpg') ?>" alt="Second slide">
-			  </div>
-                <div class="carousel-item">
-                <img class="d-block img-fluid" src="<?php echo base_url('assets\images\4_biohazardousmaterials\bm4.jpg') ?>" alt="Second slide">
-			  </div>
-                <div class="carousel-item">
-                <img class="d-block img-fluid" src="<?php echo base_url('assets\images\4_biohazardousmaterials\bm5.jpg') ?>" alt="Second slide">
-			  </div>
-                <div class="carousel-item">
-                <img class="d-block img-fluid" src="<?php echo base_url('assets\images\4_biohazardousmaterials\bm6.jpg') ?>" alt="Second slide">
-			  </div>
-                <div class="carousel-item">
-                <img class="d-block img-fluid" src="<?php echo base_url('assets\images\4_biohazardousmaterials\bm7.jpg') ?>" alt="Second slide">
-			  </div>
-            </div>
-            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-              <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-              <span class="carousel-control-next-icon" aria-hidden="true"></span>
-              <span class="sr-only">Next</span>
-            </a>
-          </div>				
-				</div>
+			
 		</div>
 	</div>					
 	</div>
